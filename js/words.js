92e0b0cb416648243ea14994b23d2ac87f90ffaf
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-09-16 12:27:50 +0100

    init

---
+++ js/words.js
@@ -0,0 +1,150 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "fragile",
+        hint: "That can be easily broken",
+      },
+      {
+        word: "memorize",
+        hint: "To learn something by heart",
+      },
+      {
+        word: "miser",
+        hint: "One that hardly spends money",
+      },
+      {
+        word: "legible",
+        hint: "That can be read",
+      },
+      {
+        word: "recite",
+        hint: "To quote somethimg off hand",
+      },
+      {
+        word: "immortal",
+        hint: "Living forever (never dying)",
+      },
+      {
+        word: "biennial",
+        hint: "Occuring once in two years",
+      },
+      {
+        word: "biannual",
+        hint: "Occurng twice in a year",
+      },
+      {
+        word: "precise",
+        hint: "Brief, and to the point",
+      },
+      {
+        word: "edible",
+        hint: "That is fit for food",
+      },
+      {
+        word: "legitimate",
+        hint: "One that is born of married parents",
+      },
+      {
+        word: "incredible",
+        hint: "Difficult to believe",
+      },
+      {
+        word: "visible",
+        hint: "That which can be seen",
+      },
+      {
+        word: "literate",
+        hint: "Ability to read",
+      },
+      {
+        word: "obscure",
+        hint: "Not easily or clearly seen or understood",
+      },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "bugs",
+        hint: "problems faced by programmers"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "github",
+        hint: "famous code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+]
